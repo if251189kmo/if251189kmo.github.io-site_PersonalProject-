c739c07f532c6f95de3147e926467024636d5e0b
if251189kmo <33289750+if251189kmo@users.noreply.github.com>
2018-08-27 16:33:43 +0300

    анімашка

--- js/script.js
+++ js/script.js
@@ -2,35 +2,24 @@ window.onload = function () {
     openMenu();
 }
 
-function openMenu() {
-    
+function openMenu() {  
     var toggleMenu = document.querySelector('.menuOpen'); // кнопка меню
     var menu = document.querySelector('.navigation'); // саме немю навігації
 
-    toggleMenu.onclick = function () {
-        
-        if (menu.classList.contains("navigation")) {
-            
+    toggleMenu.onclick = function () {       
+        if (menu.classList.contains("navigation")) {    
             menu.classList.remove('navigation');
-            menu.classList.add('activeNav');
-            
+            menu.classList.add('activeNav');           
             toggleMenu.classList.add('menuOpen-aktive');
-            
-            
+                       
             setTimeout(function(){ window.scrollTo( 0, 150 ) });
-
-
         }
-
         else {
-
             menu.classList.remove('activeNav');
-            menu.classList.add('navigation');
-            
+            menu.classList.add('navigation');            
             toggleMenu.classList.remove('menuOpen-aktive');
             
-            setTimeout(function(){ window.scrollTo( 0, 0 ) });
-            
+            setTimeout(function(){ window.scrollTo( 0, 0 ) });            
         }                     
     };
 };
